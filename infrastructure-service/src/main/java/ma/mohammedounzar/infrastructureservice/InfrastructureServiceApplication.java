9241e4f07e1591b6352d0ec804cb7ed005f528d8
mohammedounzar <ounzarmohammed@gmail.com>
2025-07-24 12:38:04 +0100

    added an infrastucture service so promotheus can be more flexible

---
+++ infrastructure-service/src/main/java/ma/mohammedounzar/infrastructureservice/InfrastructureServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.mohammedounzar.infrastructureservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrastructureServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfrastructureServiceApplication.class, args);
+    }
+
+}
---
+++ infrastructure-service/src/test/java/ma/mohammedounzar/infrastructureservice/InfrastructureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.mohammedounzar.infrastructureservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrastructureServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
